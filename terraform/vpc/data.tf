48a68894ccd64a5a10d06a7ff65a0a568d26bda5
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-09-30 19:20:28 -0500

    debug

--- terraform/vpc/data.tf
+++ terraform/vpc/data.tf
@@ -3,5 +3,5 @@
 # }
 
 locals {
-  aws_availability_zones = ["us-west-2a","us-west-2b","us-west-2c","us-west-2d","us-west-2e"]
+  aws_availability_zones = slice(["us-west-2a","us-west-2b","us-west-2c","us-west-2d","us-west-2e"], 0, length(var.cidr_block_pri))
 }
